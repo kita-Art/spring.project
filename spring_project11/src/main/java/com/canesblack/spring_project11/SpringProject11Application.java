48adf5c9ae6a0f3bcb43b956579ea8cb0d1d437b
Art Kim <art.kim@example.com>
2025-08-06 12:38:34 +0900

    [2025.08.06-1]

---
+++ spring_project11/src/main/java/com/canesblack/spring_project11/SpringProject11Application.java
@@ -0,0 +1,13 @@
+package com.canesblack.spring_project11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProject11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProject11Application.class, args);
+	}
+
+}
---
+++ spring_project11/src/test/java/com/canesblack/spring_project11/SpringProject11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canesblack.spring_project11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProject11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
